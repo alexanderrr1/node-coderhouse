86e968a4f506b51405c7d121c53c305a7c01acf3
Alexander Reineck <alexreineck@gmail.com>
2022-03-27 08:07:50 -0300

    1er Commit

--- routes/productos.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-
-const router = Router();
-
-const { productosGet } = require('../controllers/productos');
-
-router.get( '/', productosGet );
-
-module.exports = router;
